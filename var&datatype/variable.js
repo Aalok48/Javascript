1fca79aa89e66c4148b61f7c6d069a7f42e7bc6f
Alok <karnaalok48@gmail.com>
2024-07-31 15:42:01 +0545

    variables

---
+++ var&datatype/variable.js
@@ -0,0 +1,11 @@
+fullName = 'Alok karn'
+console.log(fullName)
+
+age = 21
+console.log(age+'1')  // this gives 211 because of datatype conversion
+
+x = null
+y = undefined
+
+is_good = false, true
+console.log(is_good)
