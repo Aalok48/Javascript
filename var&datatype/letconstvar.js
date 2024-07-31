b5a2d0bd3b299b5e190701d8e0772fda516bca34
Alok <karnaalok48@gmail.com>
2024-07-31 15:41:34 +0545

    let const var in js

---
+++ var&datatype/letconstvar.js
@@ -0,0 +1,13 @@
+// var: variable can be re-declared and updated. A global scope variable
+// var is not recommended to use
+
+// let: variable cannot be re-declared but updated. A block scope variable
+let fullName = "Alok karn";
+console.log(fullName);
+
+// const: variable cannot be re-declared and updated.A block scopre variable
+const age =  24;
+console.log(age);
+
+let a;
+console.log(a);
